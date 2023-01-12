9e5d290fb0a3af6ca8c1d31aff5bef293ac0a270
n-ao <maroon.nopia@gmail.com>
2023-01-12 22:14:01 +0900

    [mod] 再度base設定有効化

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://n-ao.github.io',
-    // base: '/astro-getting-started',
+    base: '/astro-getting-started',
 });
