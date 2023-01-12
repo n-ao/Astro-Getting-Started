780e49e5bcea600ec7718f04deaafbe6f35c4f04
tongullman <naoyuki.hirohashi@tongullman.co.jp>
2023-01-12 22:02:10 +0900

    [add] site, base設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://n-ao.github.io',
+    base: '/astro-getting-started',
+});
